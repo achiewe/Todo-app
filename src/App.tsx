3e7d6b544bca4149d7dd8c6ee5817cbb90ac4a97
achiewe <mamukashviliachi706@gmail.com>
2023-06-09 19:14:05 +0400

    add logic in app.tsx component when change mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import GlobalStyles from "./GlobalStyles";
-import BgMobileLight from "../src/assets/bg-mobile-light.jpg";
+import BgMobileLight from "./assets/bg-mobile-light.jpg";
+// import BgMobileDark from "./assets/bg-mobile-dark.jpg";
 import Header from "./components/Header";
 import Todo from "./components/Todo";
 import { useSelector } from "react-redux";
@@ -9,7 +10,7 @@ function App() {
   const darkMode = useSelector((redux: Mode) => redux.Mode.gloomy);
 
   return (
-    <MainContainer>
+    <MainContainer darkMode={darkMode}>
       <GlobalStyles />
       <Header />
       <Todo />
@@ -17,7 +18,7 @@ function App() {
   );
 }
 
-const MainContainer = styled.div`
+const MainContainer = styled.div<{ darkMode: boolean }>`
   width: 100%;
   min-height: 100vh;
   display: flex;
@@ -25,10 +26,11 @@ const MainContainer = styled.div`
   align-items: flex-start;
   justify-content: flex-start;
   background-image: url(${BgMobileLight});
+
   padding: 0 24px;
   background-repeat: no-repeat;
   background-size: 100% 200px;
-  background-color: #fafafa;
+  background-color: ${(props) => (props.darkMode ? "#171823" : "#FAFAFA")};
 `;
 
 export default App;
