8b495cea89a6a8b95268329ac1d7ac1f3ba796a5
achiewe <mamukashviliachi706@gmail.com>
2023-06-09 19:22:24 +0400

    add function or onclick when change the darkMode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 import TodoSvg from "../assets/TODO.svg";
+import { useDispatch, useSelector } from "react-redux";
+import iconSun from "../assets/icon-sun.svg";
+import iconMoon from "../assets/icon-moon.svg";
+import { Mode } from "../store/redux";
+import { dark } from "../store/ModeSlice";
 
 const Header = (): JSX.Element => {
+  const darkMode = useSelector((redux: Mode) => redux.Mode.gloomy);
+  const dispatch = useDispatch();
+  const clickOnMode = (): void => {
+    dispatch(dark(!darkMode));
+  };
   return (
     <HeaderMain>
       <img src={TodoSvg} alt="todo svg" />
+      <img onClick={clickOnMode} src={darkMode ? iconSun : iconMoon} />
     </HeaderMain>
   );
 };
