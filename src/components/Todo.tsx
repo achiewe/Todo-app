d378d0a8076d22c5d64daf75a3e3ab3a814e42ae
achiewe <mamukashviliachi706@gmail.com>
2023-06-09 12:53:55 +0400

    add items div

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { TodoRoot } from "../store/redux";
 import { makeTodo } from "../store/TodoSlice";
+import crossSvg from "../assets/icon-cross.svg";
 
 const Todo = (): JSX.Element => {
   const tasks = useSelector((redux: TodoRoot) => redux.createTodo);
@@ -22,12 +23,22 @@ const Todo = (): JSX.Element => {
       <form onSubmit={ClickOnSubmit}>
         <button type="submit"></button>
         <input
-          //   onChange={(e) => {
-          //     setTakeText(e.target.value);
-          //   }}
+          onChange={(e) => {
+            setTakeText(e.target.value);
+          }}
           placeholder="Create a new todo…"
         />
       </form>
+      <ul className="itemsUl">
+        <li className="textLi">
+          <div className="circleText">
+            <button className="circle"></button>
+            <h3> Jog around the park 3x</h3>
+          </div>
+          <img className="cross-svg" src={crossSvg} alt="cross svg" />
+        </li>
+        <hr />
+      </ul>
     </TodoMain>
   );
 };
@@ -74,6 +85,63 @@ const TodoMain = styled.div`
 
     input::placeholder {
       color: #9495a5;
+    }
+  }
+
+  .itemsUl {
+    width: 327px;
+    display: flex;
+    flex-direction: column;
+    background-color: #ffffff;
+    box-shadow: 0px 35px 50px -15px rgba(194, 195, 214, 0.5);
+    border-radius: 5px;
+    padding: 16px 0 22px 0;
+    gap: 16px;
+
+    .textLi {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      padding: 0 20px;
+      align-items: center;
+      justify-content: space-between;
+
+      .circleText {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 12px;
+
+        .circle {
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          border: none;
+          border: 1px solid #e3e4f1;
+          background: #ffffff;
+        }
+
+        h3 {
+          font-size: 12px;
+          font-weight: 400;
+          line-height: 12px;
+          letter-spacing: -0.1666666716337204px;
+          text-align: left;
+          color: #494c6b;
+        }
+      }
+
+      .cross-svg {
+        width: 11.79px;
+        height: 11.79px;
+      }
+    }
+
+    hr {
+      width: 100%;
+      background-color: #e3e4f1;
+      border: none;
+      height: 1px;
     }
   }
 `;
